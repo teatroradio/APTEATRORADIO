6a358b8235dca94b1c3f64fdc4f503e29285b50a
teatroradio <133716277+teatroradio@users.noreply.github.com>
2023-05-15 23:36:49 -0300

    Cmb

--- ProyectoTeatroRadio/src/app/app.module.ts
+++ ProyectoTeatroRadio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    AcercadeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # APTEATRORADIO
  TeatroRadio2023
+<body> <h1> Matías Alejandro </h1> </body>
